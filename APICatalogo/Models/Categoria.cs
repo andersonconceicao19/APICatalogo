f6abc0282990f266b828f44541a1ec265ee69d1e
Anderson Conceição <andersonconceiicao@gmail.com>
2020-06-24 14:45:16 -0300

    Iniciando proj e criando os modelos de negocio

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
