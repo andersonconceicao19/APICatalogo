11bc83832e188903a7ec84511e919206198b3e9c
Anderson Conceicao <andersonconceiicao@gmail.com>
2020-06-26 21:33:17 -0300

    Adicionando Interfaces e Migrations

---
+++ APICatalogo/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Repositories.Interfaces
+{
+    public interface ICategoriaRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task<IEnumerable<Categoria>> ObterTodasCategorias();
+    }
+}
---
+++ APICatalogo/Repositories/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Repositories.Interfaces
+{
+    public interface IProdutoRepository<TEntity>: IDisposable where TEntity : Entity
+    {
+        Task<IEnumerable<Produto>> ObterTodos();
+        Task<Produto> ObterPorId(Guid id);
+        Task<Produto> Adicionar(TEntity entity);
+        Task<Produto> Remover(Guid id);
+        Task<Produto> Atualizar(TEntity entity);
+
+    }
+}
